225f60a31280e95e1319bcc8765a4ad145612a8b
Elena Poelman <elena.poelman@redpencil.io>
2024-09-17 17:39:32 +0200

    fix inverse in case model

--- app/models/case.js
+++ app/models/case.js
@@ -3,6 +3,6 @@ import Model, { belongsTo, hasMany, attr } from '@ember-data/model';
 export default class CaseModel extends Model {
   @attr uri;
 
-  @belongsTo('event', { inverse: 'case', async: true }) event;
+  @belongsTo('event', { inverse: 'cases', async: true }) event;
   @hasMany('submission', { inverse: 'case', async: true }) submissions;
 }
