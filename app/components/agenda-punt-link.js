48fc04c9155d2ebd5e3cc66fcdf4d4854bfcfc0e
Elena Poelman <elena.poelman@redpencil.io>
2024-09-18 12:08:28 +0200

    fix agendapunt-link component

--- app/components/agenda-punt-link.js
+++ app/components/agenda-punt-link.js
@@ -1,20 +1,21 @@
 import Component from '@glimmer/component';
+import { trackedFunction } from 'ember-resources/util/function';
 import {
   DRAFT_STATUS_ID,
   PUBLISHED_STATUS_ID,
   PLANNED_STATUS_ID,
 } from 'frontend-gelinkt-notuleren/utils/constants';
 export default class AgendaPuntLinkComponent extends Component {
-  get editorStatus() {
-    const statusId = this.args.documentContainer.status.get('id');
-    if (statusId == DRAFT_STATUS_ID) {
+  editorStatus = trackedFunction(this, async () => {
+    const status = await this.args.documentContainer.get('status');
+    if (status.id == DRAFT_STATUS_ID) {
       return 'draft';
-    } else if (statusId == PLANNED_STATUS_ID) {
+    } else if (status.id == PLANNED_STATUS_ID) {
       return 'planned';
-    } else if (statusId == PUBLISHED_STATUS_ID) {
+    } else if (status.id == PUBLISHED_STATUS_ID) {
       return 'published';
     } else {
       return 'unknown';
     }
-  }
+  });
 }
